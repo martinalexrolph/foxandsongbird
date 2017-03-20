8466ffcff77df43cc42993d9597d1ee0cfb95515
Martin Rolph <martinalexrolph@gmail.com>
2017-03-20 11:33:03 +0100

    Image on pages

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,6 +10,9 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php
+	    the_post_thumbnail();
+	?> 
 
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
