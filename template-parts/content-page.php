6d7a81c42ef1f2a708ee33016e963c5b8c8a9252
Martin Rolph <martinalexrolph@gmail.com>
2017-03-20 15:54:44 +0100

    Portrait images

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -11,7 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
-	<?php if (has_post_thumbnail()): ?>
+	<?php if (has_post_thumbnail() and get_option( 'thumbnail_size_w' )>get_option( 'thumbnail_size_h' )): ?>
 	<div class="hero-image">
 		<?php the_post_thumbnail(); ?> 
 		<header class="entry-header">
@@ -19,6 +19,7 @@
 		</header>
 	</div>
 	<?php else: ?>
+	<?php the_post_thumbnail("large"); ?>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header>
