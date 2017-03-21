fa448524485594c50cc22b25f85c1bebc58224fd
Martin Rolph <martinalexrolph@gmail.com>
2017-03-21 12:04:38 +0100

    Sidebar

--- header.php
+++ header.php
@@ -24,30 +24,32 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'foxandsongbird' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-						<?php echo str_replace('&amp;', '<span class="light-amp">&amp;</span>', get_bloginfo( 'name' )); ?>
-					</a>
-				</h1>
-			<?php else : ?>
-				<span class="site-title h1">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-						<?php echo str_replace('&amp;', '<span class="light-amp">&amp;</span>', get_bloginfo( 'name' )); ?>
-					</a>
-				</span>
-			<?php endif;
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+		<div class="header-content-area">
+			<div class="site-branding">
+				<?php if ( is_front_page() && is_home() ) : ?>
+					<h1 class="site-title">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+							<?php echo str_replace('&amp;', '<span class="light-amp">&amp;</span>', get_bloginfo( 'name' )); ?>
+						</a>
+					</h1>
+				<?php else : ?>
+					<span class="site-title h1">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+							<?php echo str_replace('&amp;', '<span class="light-amp">&amp;</span>', get_bloginfo( 'name' )); ?>
+						</a>
+					</span>
+				<?php endif;
+				$description = get_bloginfo( 'description', 'display' );
+				if ( $description || is_customize_preview() ) : ?>
+					<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+				<?php endif; ?>
+			</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'foxandsongbird' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'foxandsongbird' ); ?></button>
+				<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
+			</nav><!-- #site-navigation -->
+		</div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
